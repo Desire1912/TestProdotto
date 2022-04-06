0c315f269d8982a8b0a6b64a6241752faf55e2c4
Desire1912 <dpanico@studenti.apuliadigitalmaker.it>
2022-04-06 09:10:00 +0200

    committo

---
+++ primorest/src/main/java/it/devlecc/primorest/PrimorestApplication.java
@@ -0,0 +1,13 @@
+package it.devlecc.primorest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimorestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimorestApplication.class, args);
+	}
+
+}
