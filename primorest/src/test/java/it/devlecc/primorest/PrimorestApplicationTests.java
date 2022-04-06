5f95c135e0059aec6cdeec799a81c55d050d4119
Desire1912 <dpanico@studenti.apuliadigitalmaker.it>
2022-04-06 09:13:20 +0200

    committo

---
+++ primorest/src/test/java/it/devlecc/primorest/PrimorestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.devlecc.primorest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimorestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
